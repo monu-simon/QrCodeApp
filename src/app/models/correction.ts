f0a82899f2cdca6b06f9b8f2dda1178de45b4130
monu-simon <monusimonm@gmail.com>
2022-07-04 16:13:23 +0530

    Added error correction levels

---
+++ src/app/models/correction.ts
@@ -0,0 +1,11 @@
+export interface CorrectonLevel {
+    title: string,
+    key: string
+}
+
+export enum CorrectonLevels {
+    LOW = 'L',
+    MEDIUM = 'M',
+    QUARTILE = 'Q',
+    HIGH = 'H'
+}
--- src/app/ui/home/home.component.ts
+++ src/app/ui/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CorrectonLevel, CorrectonLevels } from 'src/app/models/correction';
 
 @Component({
   selector: 'app-home',
@@ -7,17 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  url: string ='https://www.google.com';
+  url: string = 'https://www.google.com';
+
+  correctionLevels!: CorrectonLevel[]
+  errorLevel: any = 'H';
   constructor() { }
 
   ngOnInit(): void {
+    this.correctionLevels = [
+      {
+        title: 'LOW',
+        key: CorrectonLevels.LOW
+      },
+      {
+        title: 'MEDIUM',
+        key: CorrectonLevels.MEDIUM
+      },
+      {
+        title: 'Quartile',
+        key: CorrectonLevels.QUARTILE
+      },
+      {
+        title: 'HIGH',
+        key: CorrectonLevels.HIGH
+      }
+    ]
   }
 
   download() {
     const canvas = document.getElementsByTagName('canvas')[0];
     const image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
     var link = document.createElement('a');
-    link.download = "my-image.png";
+    link.download = "my-qrcode.png";
     link.href = image;
     link.click();
   }

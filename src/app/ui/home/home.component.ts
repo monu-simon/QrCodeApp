da3e005e3d070d33b38cfdfc4f94cba4abdacbcd
monu-simon <monusimonm@gmail.com>
2022-07-04 13:22:38 +0530

    Added download button logic  for download

--- src/app/ui/home/home.component.ts
+++ src/app/ui/home/home.component.ts
@@ -13,4 +13,13 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  download() {
+    const canvas = document.getElementsByTagName('canvas')[0];
+    const image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
+    var link = document.createElement('a');
+    link.download = "my-image.png";
+    link.href = image;
+    link.click();
+  }
+
 }
